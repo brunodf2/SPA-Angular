3491ed88dfd273ad137cee44276be198d9245f8a
Bruno Carlos Mota Araujo <BrunoAraujo@caesb.gov.br>
2019-05-31 08:28:51 -0300

    Funcionalidade de Botoes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,7 @@
 import { BrowserModule } from "@angular/platform-browser";
 import { NgModule } from "@angular/core";
+import {MatIconModule} from '@angular/material/icon';
+
 
 import { AppComponent } from "./app.component";
 
@@ -8,12 +10,16 @@ import { MatDialogModule } from "@angular/material/dialog";
 import { HeaderComponent } from './header/header.component';
 import { ContentComponent } from './content/content.component';
 import { FooterComponent } from './footer/footer.component';
+import { TooltipModule } from 'ng2-tooltip-directive';
+
 
 @NgModule({
   declarations: [AppComponent, HeaderComponent, ContentComponent, FooterComponent],
   imports: [
       BrowserModule,
       BrowserAnimationsModule,
+      MatIconModule,
+      TooltipModule,
       MatDialogModule
     ],
   providers: [],
